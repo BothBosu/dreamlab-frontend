107a4da58a97c8e435de019597b51d90bc91843e
Pitipat Gumphusiri <pitipat.both@gmail.com>
2025-03-05 12:49:52 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +13,25 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'process': 'process/browser',
+      'stream': 'stream-browserify',
+      'util': 'util'
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+          crypto: true,
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  }
 })
